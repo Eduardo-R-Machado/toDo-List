16959a07f9ae49988f559032e4d08676ccab4aea
eduardo329 <eduardo_rj_18@hotmail.com>
2022-08-09 17:52:55 -0300

    Tentando inserir index nas todos

--- todo.js
+++ todo.js
@@ -1,3 +1,13 @@
+firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            findTodos(user);
+
+        } else {
+            window.location.href = "index.html";
+        }
+    }
+);
+
 
 function openModal(){
     openModal2();
@@ -18,56 +28,112 @@ setTimeout(() => {
 }, 200);
 }
 
-firebase.auth().onAuthStateChanged(user => {
-    if (!user) {
-        window.location.href = "index.html";
-    }
-  }
-);
 
-function findTodos(){
-    firebase.firestore()
-        .collection('Todos')
-        .get()
-        .then(snapshot => {
-            snapshot.docs.forEach(doc => {
-                console.log(doc.data());
-            }   )
-        }   )
-}
 
 //function que  faz um forEach nos todos e use o form-group para criar os todos
-function createTodos(){
+
+function findTodos(user){
+    firebase.firestore()
+        .collection('Todos')
+        .where('user.uid', '==', user.uid)
+        .orderBy('checked')
+        .get()
+        .then(snapshot => {
+                const todos = snapshot.docs.map(doc => doc.data());
+                addTodosToScreen(todos);
+            })
+        .catch(error => {
+            console.log(error);
+            alert('Erro ao buscar todos');
+        });
+}
+
+function addTodosToScreen(todos){
+    const orderedList = document.querySelector('.wrp-list-todos');
+
+    todos.forEach(todo => {
+        const formGroup = document.createElement('div');
+        formGroup.className = 'form-group';
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.className = 'checkbox';
+
+        const label = document.createElement('label');
+        label.innerText = todo.todo;
+        label.className = 'label';
+        label.setAttribute('for', todo.id);
+        formGroup.appendChild(checkbox);
+        formGroup.appendChild(label);
+
+        orderedList.appendChild(formGroup);
+
+        formGroup.addEventListener('click', () => {
+            findTodosByUid(todo.id);
+        } );
+    });
+}
+
+//criando função que salva os todos no firebase
+function saveTodo(){
+    const todo = createTodo();
+        save(todo);
+}
+
+function createTodo(){
+    return {
+        todo: document.querySelector('#novaTarefa').value,
+        checked: false,
+        indexTodo: indexTodo(index),
+        user: {
+            uid: firebase.auth().currentUser.uid,
+        }
+    }
+}
+
+function save(todo) {
+    firebase.firestore()
+        .collection('Todos')
+        .add(todo)
+        .then(() => {
+            alert('Tarefa salva com sucesso');
+            closeModal();
+            reloadTodos();
+        })
+        .catch(error => {
+
+            console.log(error);
+            alert('Erro ao salvar tarefa');
+        }   );
+}
+
+function update(todo) {
+    firebase.firestore()
+        .collection('Todos')
+        .doc(getTodoUid())
+        .update(todo)
+        .then(() => {
+            alert('Tarefa atualizada com sucesso');
+            closeModal();
+            reloadTodos();
+        }).catch(error => {
+            console.log(error);
+            alert('Erro ao atualizar tarefa');
+        }   );
+}
+
+function reloadTodos() {
+    const orderedList = document.querySelector('.wrp-list-todos');
+    orderedList.innerHTML = '';
+    document.querySelector('#novaTarefa').value = '';
+    findTodos(firebase.auth().currentUser);
+}
+
+function indexTodo() {
     firebase.firestore()
         .collection('Todos')
         .get()
         .then(snapshot => {
-            snapshot.docs.forEach(doc => {
-                let todo = doc.data();
-                let todoDiv = document.createElement('div');
-                todoDiv.classList.add('form-group');
-                todoDiv.innerHTML = `
-                <div class="form-group">
-                    <input class="checkbox" type="checkbox" value="" id="defaultCheck1">
-                    <label >
-                        ${todo.todo}
-                    </label>
-                    
-                </div>
-                `;
-                document.querySelector('.wrp-list-todos').appendChild(todoDiv);
-            }   )
-        }   )
+            const todos = snapshot.docs.map(doc => doc.data());
+            let index = todos.length;
+        } )
 }
-
-
-createTodos();
-
-
-
-
-
-
-
-
-
